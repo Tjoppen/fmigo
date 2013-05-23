fa821c271554bb66c9de201383d07ca9fc52f5c4
steffe <steffe@2c2fc05c-dec6-c2ab-fb02-ff1293c985b9>
2013-05-23 10:21:39 +0000

    Added fmu-builder
    git-svn-id: svn://svn.cs.umu.se/2015/scania-vtb/scania-vtb@132 2c2fc05c-dec6-c2ab-fb02-ff1293c985b9

---
+++ fmu-builder/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='fmu-builder',
+    version='0.0.1',
+    description='Build FMUs simply via command line.',
+    author='Stefan Hedman',
+    author_email='schteppe@gmail.com',
+    scripts=['bin/fmu-builder']
+    )

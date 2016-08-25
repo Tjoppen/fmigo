22cae595ec15a73c13ce0beaa7000f7120c4aca7
Tomas Härdin <tomas.hardin@umu.se>
2016-08-25 16:54:13 +0200

    Add chained_sho example
    It seems we can sometimes get away with zero damping on the coupling springs.
    This seems to introduce some limitations though. For instance, we can't use the EPCE filter or crank k_c too high.

--- epce_plots.sh
+++ epce_plots.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-cd gsl2/coupled_sho
-
-run_test() {
+run_coupled_sho() {
     N=$1
     dt=$2
     omega=$(python <<< "print($N * $3)")
@@ -46,11 +44,96 @@ run_test() {
     rm temp.csv
 }
 
-run_test 16 0.1  0.1 1 2 out-on-0.1.pdf
-run_test 16 0.1  1   1 2 out-on-1.pdf
-run_test 16 0.1 10   1 2 out-on-10.pdf
-run_test 16 0.1  0.1 1 0 out-off-0.1.pdf
-run_test 16 0.1  1   1 0 out-off-1.pdf
-run_test 16 0.1 10   1 0 out-off-10.pdf
+pushd gsl2/coupled_sho
 
+#run_coupled_sho 16 0.1  0.1 1 2 out-on-0.1.pdf
+#run_coupled_sho 16 0.1  1   1 2 out-on-1.pdf
+#run_coupled_sho 16 0.1 10   1 2 out-on-10.pdf
+#run_coupled_sho 16 0.1  0.1 1 0 out-off-0.1.pdf
+#run_coupled_sho 16 0.1  1   1 0 out-off-1.pdf
+#run_coupled_sho 16 0.1 10   1 0 out-off-10.pdf
 
+popd
+
+run_chained_sho() {
+    N=$1
+
+    # dt must decrease with N since k_c increases with N
+    dt=$(python <<< "print($2 / $N)")
+    #dt=$2
+
+    filter_length=$3
+    output_filename=$4
+    echo $dt
+
+    mass=$(python <<< "print(1.0 / $N)")
+    k_i=$(python <<< "print(1.0 / $N)")
+    damping_i=$(python <<< "print(1.0 / $N)")
+    k_c=$(python <<< "print(10*$N)")
+
+    # It seems that we don't want damping in the coupling springs,
+    # which kind of makes sense. If we did we'd lose energy
+    damping_c=0
+    #damping_c=$(python <<< "print(10/$N)")
+    #damping_c=1
+    #damping_c=$(python <<< "print(10*$N)")
+
+    tmax=40
+
+    amp=$(python <<< "print(1.0 / $dt)")
+    start=$(python <<< "print(int(0.0 / $dt))")
+    length=$(python <<< "print(int(20.0 / $dt))")
+
+    FMUS=": -np 1 fmi-mpi-server ../../impulse/impulse.fmu"
+    # Connect impulse generator as force input on last system
+    CONNS="-c 0,0,$N,6"
+    PARAMS="-p i,0,4,0:i,0,5,${start}:i,0,6,${length}:0,7,1"
+    # Disable coupling spring on first system
+    PARAMS="${PARAMS} -p 1,1,0:1,2,0"
+    # Dump state of first system in sho.m
+    PARAMS="${PARAMS} -p b,1,97,true"
+
+    for i in $(seq 1 $N)
+    do
+        i2=$(bc <<< "$i + 1")
+        if [ $i -lt $N ]
+        then
+            CONNS="${CONNS} -c $i,12,$i2,5:$i2,10,$i,6"
+        fi
+
+        if [ $i -ne 1 ]
+        then
+            # Set coupling springs except for the first system which has none
+            PARAMS="${PARAMS} -p $i,1,${k_c}:$i,2,${damping_c}"
+        fi
+
+        PARAMS="${PARAMS} -p i,$i,98,${filter_length} -p $i,0,${mass}:$i,4,${damping_i}:$i,95,${k_i}:$i,7,0"
+        FMUS="${FMUS} : -np 1 fmi-mpi-server chained_sho.fmu"
+    done
+
+    mpiexec -np 1 fmi-mpi-master -m gs -t ${tmax} -d $dt ${CONNS} ${PARAMS} ${FMUS} > ${output_filename} || true
+    # Cut out mpi crap if GSL exploded
+    sed -i '/---.*/{N;N;N;N;N;N;N;N;N;N;s/---.*//}' ${output_filename}
+}
+
+pushd gsl2/chained_sho
+
+run_chained_sho 1  0.5 0 out-1.csv
+run_chained_sho 4  0.5 0 out-4.csv
+
+octave --no-gui --persist --eval "
+        d=csvread('out-1.csv');
+
+        % This is the force impulse
+        plot(d(:,1),d(:,2),'k-')
+        hold on;
+
+        % This is the N=1 system's behavior
+        plot(d(:,1),d(:,6),'kx-')
+
+        % Finally, plot the N=4 system
+        d=csvread('out-4.csv');
+        plot(d(:,1),d(:,6:4:end),'.-');
+"
+
+popd

b1efbadb9c4141e513b941b48b9f73391a023641
Tomas Härdin <tomas.hardin@umu.se>
2016-09-22 14:55:33 +0200

    Replace ugly build script with CMake based system
    Because CMake is only a meta build system we need to put the CMakeLists.txt generation in a script that we call every time we make a new FMU

---
+++ generate_everything.sh
@@ -0,0 +1,61 @@
+set -e
+
+MD2HDR="`pwd`/../fmu-builder/bin/modeldescription2header"
+GENERATOR="`pwd`/../fmu-builder/bin/cmake-generator -t `pwd`/templates/fmi2/ -i `pwd`/../FMILibrary-2.0.1/ThirdParty/FMI/default"
+
+GSLFMUS="
+    gsl2/clutch2
+    gsl2/chained_sho
+    gsl2/exp
+    gsl2/clutch_ef
+    gsl2/clutch
+    gsl2/coupled_sho
+    gsl2/mass_force
+    gsl2/mass_force_fe
+"
+
+FMUS="
+    typeconvtest
+    impulse
+    lumpedrod
+    kinematictruck/body
+    kinematictruck/engine
+    kinematictruck/gearbox2
+    kinematictruck/clutch
+    forcevelocitytruck/body
+    forcevelocitytruck/gearbox
+"
+
+cat <<END>CMakeLists.txt
+cmake_minimum_required(VERSION 2.8)
+
+#Getting GSL to run on Windows is too much of a hassle right now
+#TODO: use msys2?
+if (NOT WIN32)
+END
+
+# Warnings during compilation may go unnoticed without -Werror, leading to
+# hard-to-debug problems
+export CFLAGS="-Wall -Werror -O3"
+
+#New GSL interface
+for d in $GSLFMUS
+do
+    echo "    add_subdirectory($d)" >> CMakeLists.txt
+    GSL="-t `pwd`/templates/gsl2/gsl-interface.c -t `pwd`/templates/gsl2/gsl-interface.h -l gsl,gslcblas,m"
+    pushd $d
+        python ${MD2HDR} modelDescription.xml > sources/modelDescription.h
+        python ${GENERATOR} ${GSL}
+    popd
+done
+
+echo "endif ()" >> CMakeLists.txt
+
+for d in $FMUS
+do
+    echo "add_subdirectory($d)" >> CMakeLists.txt
+    pushd $d
+        python ${MD2HDR} modelDescription.xml > sources/modelDescription.h
+        python ${GENERATOR}
+    popd
+done

64b7ab0f5c97d62f57d83af719db851350890c10
Tomas Härdin <tomas.hardin@umu.se>
2016-09-27 15:13:24 +0200

    Newly generated things

--- gsl2/trailer/sources/modelDescription.h
+++ gsl2/trailer/sources/modelDescription.h
@@ -6,7 +6,7 @@
 #define MODEL_GUID "{ca64d56e-cc83-44bb-ad78-afbe219a947c}"
 #define FMI_COSIMULATION
 #define HAVE_DIRECTIONAL_DERIVATIVE 0
-#define NUMBER_OF_REALS 31
+#define NUMBER_OF_REALS 36
 #define NUMBER_OF_INTEGERS 1
 #define NUMBER_OF_BOOLEANS 0
 #define NUMBER_OF_STATES 0
@@ -27,25 +27,30 @@ typedef struct {
     fmi2Real c_r_0; //VR=10
     fmi2Real c_r_1; //VR=11
     fmi2Real mu; //VR=12
-    fmi2Real k; //VR=13
-    fmi2Real gamma; //VR=14
-    fmi2Real integrator; //VR=15
-    fmi2Real integrate_dx_e; //VR=16
+    fmi2Real k_d; //VR=13
+    fmi2Real gamma_d; //VR=14
+    fmi2Real k_t; //VR=15
+    fmi2Real gamma_t; //VR=16
     fmi2Real integrate_d_omega; //VR=17
-    fmi2Real phi_i; //VR=18
-    fmi2Real omega_i; //VR=19
-    fmi2Real tau_d; //VR=20
-    fmi2Real tau_e; //VR=21
-    fmi2Real alpha; //VR=22
-    fmi2Real brake; //VR=23
-    fmi2Real x; //VR=24
-    fmi2Real v; //VR=25
-    fmi2Real a; //VR=26
-    fmi2Real tau_c; //VR=27
-    fmi2Real f_c; //VR=28
-    fmi2Real x_in; //VR=1001
-    fmi2Real v_in; //VR=1002
-    fmi2Real f_in; //VR=1003
+    fmi2Real integrate_dx_e; //VR=18
+    fmi2Real integrator; //VR=19
+    fmi2Real phi_i; //VR=20
+    fmi2Real omega_i; //VR=21
+    fmi2Real tau_d; //VR=22
+    fmi2Real tau_e; //VR=23
+    fmi2Real angle; //VR=24
+    fmi2Real brake; //VR=25
+    fmi2Real x_in; //VR=26
+    fmi2Real v_in; //VR=27
+    fmi2Real f_in; //VR=28
+    fmi2Real x; //VR=29
+    fmi2Real v; //VR=30
+    fmi2Real a; //VR=31
+    fmi2Real phi; //VR=32
+    fmi2Real omega; //VR=33
+    fmi2Real alpha; //VR=34
+    fmi2Real tau_c; //VR=35
+    fmi2Real f_c; //VR=36
     fmi2Integer filter_length; //VR=98
 
 } modelDescription_t;
@@ -65,25 +70,30 @@ static const modelDescription_t defaults = {
     0.0, //c_r_0
     0.0, //c_r_1
     0.0, //mu
-    0.0, //k
-    0.0, //gamma
-    0.0, //integrator
-    0.0, //integrate_dx_e
+    0.0, //k_d
+    0.0, //gamma_d
+    0.0, //k_t
+    0.0, //gamma_t
     0.0, //integrate_d_omega
+    0.0, //integrate_dx_e
+    0.0, //integrator
     0.0, //phi_i
     0.0, //omega_i
     0.0, //tau_d
     0.0, //tau_e
-    0.0, //alpha
+    0.0, //angle
     0.0, //brake
-    0, //x
-    0, //v
-    0, //a
-    0, //tau_c
-    0, //f_c
     0.0, //x_in
     0.0, //v_in
     0.0, //f_in
+    0, //x
+    0, //v
+    0, //a
+    0, //phi
+    0, //omega
+    0, //alpha
+    0, //tau_c
+    0, //f_c
     0, //filter_length
 
 };
@@ -101,25 +111,30 @@ static const modelDescription_t defaults = {
 #define VR_C_R_0 10
 #define VR_C_R_1 11
 #define VR_MU 12
-#define VR_K 13
-#define VR_GAMMA 14
-#define VR_INTEGRATOR 15
-#define VR_INTEGRATE_DX_E 16
+#define VR_K_D 13
+#define VR_GAMMA_D 14
+#define VR_K_T 15
+#define VR_GAMMA_T 16
 #define VR_INTEGRATE_D_OMEGA 17
-#define VR_PHI_I 18
-#define VR_OMEGA_I 19
-#define VR_TAU_D 20
-#define VR_TAU_E 21
-#define VR_ALPHA 22
-#define VR_BRAKE 23
-#define VR_X 24
-#define VR_V 25
-#define VR_A 26
-#define VR_TAU_C 27
-#define VR_F_C 28
-#define VR_X_IN 1001
-#define VR_V_IN 1002
-#define VR_F_IN 1003
+#define VR_INTEGRATE_DX_E 18
+#define VR_INTEGRATOR 19
+#define VR_PHI_I 20
+#define VR_OMEGA_I 21
+#define VR_TAU_D 22
+#define VR_TAU_E 23
+#define VR_ANGLE 24
+#define VR_BRAKE 25
+#define VR_X_IN 26
+#define VR_V_IN 27
+#define VR_F_IN 28
+#define VR_X 29
+#define VR_V 30
+#define VR_A 31
+#define VR_PHI 32
+#define VR_OMEGA 33
+#define VR_ALPHA 34
+#define VR_TAU_C 35
+#define VR_F_C 36
 #define VR_FILTER_LENGTH 98
 
 
@@ -144,25 +159,30 @@ static fmi2Status generated_fmi2GetReal(const modelDescription_t *md, const fmi2
         case 10: value[i] = md->c_r_0; break;
         case 11: value[i] = md->c_r_1; break;
         case 12: value[i] = md->mu; break;
-        case 13: value[i] = md->k; break;
-        case 14: value[i] = md->gamma; break;
-        case 15: value[i] = md->integrator; break;
-        case 16: value[i] = md->integrate_dx_e; break;
+        case 13: value[i] = md->k_d; break;
+        case 14: value[i] = md->gamma_d; break;
+        case 15: value[i] = md->k_t; break;
+        case 16: value[i] = md->gamma_t; break;
         case 17: value[i] = md->integrate_d_omega; break;
-        case 18: value[i] = md->phi_i; break;
-        case 19: value[i] = md->omega_i; break;
-        case 20: value[i] = md->tau_d; break;
-        case 21: value[i] = md->tau_e; break;
-        case 22: value[i] = md->alpha; break;
-        case 23: value[i] = md->brake; break;
-        case 24: value[i] = md->x; break;
-        case 25: value[i] = md->v; break;
-        case 26: value[i] = md->a; break;
-        case 27: value[i] = md->tau_c; break;
-        case 28: value[i] = md->f_c; break;
-        case 1001: value[i] = md->x_in; break;
-        case 1002: value[i] = md->v_in; break;
-        case 1003: value[i] = md->f_in; break;
+        case 18: value[i] = md->integrate_dx_e; break;
+        case 19: value[i] = md->integrator; break;
+        case 20: value[i] = md->phi_i; break;
+        case 21: value[i] = md->omega_i; break;
+        case 22: value[i] = md->tau_d; break;
+        case 23: value[i] = md->tau_e; break;
+        case 24: value[i] = md->angle; break;
+        case 25: value[i] = md->brake; break;
+        case 26: value[i] = md->x_in; break;
+        case 27: value[i] = md->v_in; break;
+        case 28: value[i] = md->f_in; break;
+        case 29: value[i] = md->x; break;
+        case 30: value[i] = md->v; break;
+        case 31: value[i] = md->a; break;
+        case 32: value[i] = md->phi; break;
+        case 33: value[i] = md->omega; break;
+        case 34: value[i] = md->alpha; break;
+        case 35: value[i] = md->tau_c; break;
+        case 36: value[i] = md->f_c; break;
         default: return fmi2Error;
         }
     }
@@ -185,25 +205,30 @@ static fmi2Status generated_fmi2SetReal(modelDescription_t *md, const fmi2ValueR
         case 10: md->c_r_0 = value[i]; break;
         case 11: md->c_r_1 = value[i]; break;
         case 12: md->mu = value[i]; break;
-        case 13: md->k = value[i]; break;
-        case 14: md->gamma = value[i]; break;
-        case 15: md->integrator = value[i]; break;
-        case 16: md->integrate_dx_e = value[i]; break;
+        case 13: md->k_d = value[i]; break;
+        case 14: md->gamma_d = value[i]; break;
+        case 15: md->k_t = value[i]; break;
+        case 16: md->gamma_t = value[i]; break;
         case 17: md->integrate_d_omega = value[i]; break;
-        case 18: md->phi_i = value[i]; break;
-        case 19: md->omega_i = value[i]; break;
-        case 20: md->tau_d = value[i]; break;
-        case 21: md->tau_e = value[i]; break;
-        case 22: md->alpha = value[i]; break;
-        case 23: md->brake = value[i]; break;
-        case 24: md->x = value[i]; break;
-        case 25: md->v = value[i]; break;
-        case 26: md->a = value[i]; break;
-        case 27: md->tau_c = value[i]; break;
-        case 28: md->f_c = value[i]; break;
-        case 1001: md->x_in = value[i]; break;
-        case 1002: md->v_in = value[i]; break;
-        case 1003: md->f_in = value[i]; break;
+        case 18: md->integrate_dx_e = value[i]; break;
+        case 19: md->integrator = value[i]; break;
+        case 20: md->phi_i = value[i]; break;
+        case 21: md->omega_i = value[i]; break;
+        case 22: md->tau_d = value[i]; break;
+        case 23: md->tau_e = value[i]; break;
+        case 24: md->angle = value[i]; break;
+        case 25: md->brake = value[i]; break;
+        case 26: md->x_in = value[i]; break;
+        case 27: md->v_in = value[i]; break;
+        case 28: md->f_in = value[i]; break;
+        case 29: md->x = value[i]; break;
+        case 30: md->v = value[i]; break;
+        case 31: md->a = value[i]; break;
+        case 32: md->phi = value[i]; break;
+        case 33: md->omega = value[i]; break;
+        case 34: md->alpha = value[i]; break;
+        case 35: md->tau_c = value[i]; break;
+        case 36: md->f_c = value[i]; break;
         default: return fmi2Error;
         }
     }

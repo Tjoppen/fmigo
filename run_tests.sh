dbd74b45d97ab3861f48e7c7d33b83ed15dd98a0
Tomas Härdin <tomas.hardin@umu.se>
2016-12-16 14:26:16 +0100

    Add tests of loop solver

--- run_tests.sh
+++ run_tests.sh
@@ -20,6 +20,8 @@ else
 fi
 
 (cd articles/work-reports       && ( ./run_tests.sh  || ( echo "failed tests in work-reports" && exit -1 ) ) )
+(cd articles/truck              && ( python run_gsl_trucks.py --test  || ( echo "failed loop solver test" && exit -1 ) ) )
 (cd fmu-examples/co_simulation  && ( ./run_tests.sh ||  ( echo "failted test in co_simulation" && exit -1 ) ) )
 (cd ${FMUS_DIR}/typeconvtest    && ( ./test_typeconv.sh ||  ( echo "failed tests in typeconvtest" && exit -1 ) ) )
+(cd ${FMUS_DIR}/loopsolvetest   && ( ./test_loops.sh ||  ( echo "failed tests in loopsolvetest" && exit -1 ) ) )
 echo All tests OK

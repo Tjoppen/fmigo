1fab5ed4f7f5c8e176c07f0bbb059b2c35771e16
Tomas Härdin <tomas.hardin@umu.se>
2016-12-28 15:19:31 +0100

    Mess around with the boilerplate, change output of run_timings.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,23 +1,6 @@
 # This runs all test scripts we have scattered all over the place
 set -e
-
-export BUILD_DIR=$(pwd)/build
-export FMUS_DIR=$(pwd)/umit-fmus
-export PATH=$PATH:$BUILD_DIR/install/bin
-export SERVER=fmi-tcp-server
-export MASTER=fmi-tcp-master
-export MPI_SERVER=fmi-mpi-server
-export MPI_MASTER=fmi-mpi-master
-
-if [[ "`uname`" = "Windows_NT" || "`uname`" = "MINGW64"* ]]
-then
-    export WIN=1
-else
-    export WIN=0
-    #Produce core dumps
-    ulimit -c unlimited
-    ulimit -n 2048  #should be some value greater than 2*max(Nseq), probably a power of two too
-fi
+source boilerplate.sh
 
 (cd articles/work-reports       && ( ./run_tests.sh  || ( echo "failed tests in work-reports" && exit -1 ) ) )
 (cd articles/truck              && ( python run_gsl_trucks.py --test  || ( echo "failed loop solver test" && exit -1 ) ) )

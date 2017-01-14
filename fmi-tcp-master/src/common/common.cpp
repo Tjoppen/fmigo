d6599e7dda71411ec203bcb97265c661e0dcb77e
Jonas Sandqvist <s4nk13@gmail.com>
2017-01-14 11:03:10 +0100

    clean up of extract vector

--- fmi-tcp-master/src/common/common.cpp
+++ fmi-tcp-master/src/common/common.cpp
@@ -31,20 +31,19 @@ string int_to_string(int i){
     return ss.str();
 }
 
-void extract_vector(double *outVec, std::deque<std::vector<double>> *inQue){
-    if(inQue->size() == 0) return;
+void extract_vector(double outVec[], std::deque<std::vector<double>> &inQue){
+    if(inQue.size() == 0) return;
     int i = 0;
-    for(auto v:(*inQue)[0])
+    auto q  = inQue.front();
+    for(auto v:q)
       outVec[i++] = v;
-    inQue->pop_back();
+    inQue.pop_front();
 }
 
-void extract_vector(std::vector<double> *outVec, std::deque<std::vector<double>> *inQue){
-    if(inQue->size() == 0) return;
-    outVec->clear();
-    for(auto v:(*inQue)[0])
-        outVec->push_back( v );
-    inQue->pop_back();
+void extract_vector(std::vector<double> &outVec, std::deque<std::vector<double>> &inQue){
+    if(inQue.size() == 0) return;
+    outVec = inQue.front();
+    inQue.pop_front();
 }
 
 jm_log_level_enu_t protoJMLogLevelToFmiJMLogLevel(fmitcp_proto::jm_log_level_enu_t logLevel) {

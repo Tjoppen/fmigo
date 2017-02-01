3956461c965192dc64b5064b4ff0de0b2e9d8abc
Tomas Härdin <tomas.hardin@umu.se>
2017-02-01 13:40:42 +0100

    Assume binaries are in path, not ../build/install/bin

--- lumped-test.sh
+++ lumped-test.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-FMIPATH=`pwd`/../build/install/bin
-
 TAU1=r,0,10,4
 TAU2=r,0,11,-4
 J0=r,0,16,20
@@ -16,9 +14,9 @@ KD2=r,0,21,100
 DD2=r,0,22,10
 N=i,0,28,6
 mpirun \
-    -np  1 $FMIPATH/fmi-mpi-master\
+    -np  1 fmi-mpi-master\
     -d 0.1 \
     -p $KD1:$DD1:$KD2:$DD2:$TAU1:$TAU2:$COMPLIANCE:$RELAX:$N:$J0 : \
--np  1 $FMIPATH/fmi-mpi-server ./lumpedrod/lumpedrod.fmu  
+-np  1 fmi-mpi-server ./lumpedrod/lumpedrod.fmu
 
     

a513d0fd5e91cdd6e11d93898226fd8933779b2d
Jonas Sandqvist <s4nk13@gmail.com>
2017-02-06 15:55:45 +0100

    updates

--- gsl2/springs/sources/springs.c
+++ gsl2/springs/sources/springs.c
@@ -1,22 +1,32 @@
 #include "modelDescription.h"
 #include "gsl-interface.h"
+
+#define SIMULATION_TYPE cgsl_simulation
+#define SIMULATION_INIT springs_init
+#define SIMULATION_FREE cgsl_free_simulation
+
 #include "fmuTemplate.h"
 
-#ifndef STATES
-#define STATES {}
-#endif
 static void updateStates(modelDescription_t *md){
     md->x0 = md->x_in;
     /* md->dx0 = md->v0; */
     /* md->dv0 = -md->k1 * (md->x0 - md->x1); */
     /* md->dx1 = md->v1; */
     /* md->dv1 = -md->k1 * (md->x1 - md->x0) -md->k2 * (md->x1 - md->x_in); */
-    md->x_out = md->x1;
+    //md->x_out = md->x1;
 }
 
 // used to set the next time event, if any.
 static void eventUpdate(ModelInstance *comp, fmi2EventInfo *eventInfo) {
     return;
+}
+
+static void SIMULATION_INIT(state_t *s) {
+    return;
+}
+
+static void doStep(state_t *s, fmi2Real currentCommunicationPoint, fmi2Real communicationStepSize) {
+    cgsl_step_to( &s->simulation, currentCommunicationPoint, communicationStepSize );
 }
 
 //gcc -g springs.c ../../../templates/gsl2/gsl-interface.c -DCONSOLE -I../../../templates/gsl2 -I../../../templates/fmi2 -lgsl -lgslcblas -lm -Wall

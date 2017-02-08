7d7849900868b4da6506aa3f37fb3894288d307f
Jonas Sandqvist <s4nk13@gmail.com>
2017-02-08 16:10:07 +0100

    updateStates

--- gsl2/springs/sources/springs.c
+++ gsl2/springs/sources/springs.c
@@ -8,6 +8,7 @@
 
 #include "fmuTemplate.h"
 
+static void updateStates(modelDescription_t *md){}
 static void update_all(modelDescription_t *md){
     //std::cout << "hello " << sdt::endl;
     md->dx0 = md->v0;

462a9866fdaca4c501979ecd38f7e3e6d40d6c08
Jonas Sandqvist <s4nk13@gmail.com>
2017-02-10 14:38:38 +0100

    addapted bouncing ball to work with generated modelDescription.h

--- gsl2/bouncingBall/sources/bouncingBall.c
+++ gsl2/bouncingBall/sources/bouncingBall.c
@@ -1,3 +1,22 @@
+/* ---------------------------------------------------------------------------*
+ * Sample implementation of an FMU - a bouncing ball.
+ * This demonstrates the use of state events and reinit of states.
+ * Equations:
+ *  der(h) = v;
+ *  der(v) = -g;
+ *  when h<0 then v := -e * v;
+ *  where
+ *    h      height [m], used as state, start = 1
+ *    v      velocity of ball [m/s], used as state
+ *    der(h) velocity of ball [m/s]
+ *    der(v) acceleration of ball [m/s2]
+ *    g      acceleration of gravity [m/s2], a parameter, start = 9.81
+ *    e      a dimensionless parameter, start = 0.7
+ *
+ * Copyright QTronic GmbH. All rights reserved.
+ * ---------------------------------------------------------------------------*/
+
+// include code that implements the FMI based on the above definitions
 #include "modelDescription.h"
 #include "gsl-interface.h"
 
@@ -6,27 +25,62 @@
 #define SIMULATION_FREE cgsl_free_simulation
 
 #include "fmuTemplate.h"
+// define class name and unique id
+#define MODEL_IDENTIFIER bouncingBall
+#define MODEL_GUID "{8c4e810f-3df3-4a00-8276-176fa3c9f003}"
 
-static void updateStates(modelDescription_t *md){}
+// define initial state vector as vector of value references
 static void update_all(modelDescription_t *md){
-    //std::cout << "hello " << sdt::endl;
-    md->dx0 = md->v0;
-    md->dv0 = -md->k1 * (md->x0 - md->x1);
-    md->dx1 = md->v1;
-    md->dv1 = -md->k1 * (md->x1 - md->x0) -md->k2 * (md->x1 - md->x_in);
-    //fprintf(stderr,"%f %f %f %f \n",md->dx0,md->dv0,md->dx1,md->dv1);
+    md->der_h = md->v;
+    md->der_v = -md->g;
 }
 
-static fmi2Status getEventIndicator(const modelDescription_t *md, fmi2Real eventIndicators[]){
-
-
+// offset for event indicator, adds hysteresis and prevents z=0 at restart
+#define EPS_INDICATORS 1e-14
+fmi2Real getEventIndicator(const modelDescription_t* md, fmi2Real eventIndicators[]) {
+    eventIndicators[0] = md->h + (md->v>0 ? EPS_INDICATORS : -EPS_INDICATORS);
     return fmi2OK;
 }
 
 // used to set the next time event, if any.
 static void eventUpdate(ModelInstance *comp, fmi2EventInfo *eventInfo) {
-    return;
+    modelDescription_t* md = &comp->s.md;
+    if (md->h<0 && md->v<0) {
+        md->v = - md->e * md->v;
+    }
+    eventInfo->valuesOfContinuousStatesChanged   = fmi2True;
+    eventInfo->nominalsOfContinuousStatesChanged = fmi2False;
+    eventInfo->terminateSimulation   = fmi2False;
+    eventInfo->nextEventTimeDefined  = fmi2False;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// used to set the next time event, if any.
 
 static void SIMULATION_INIT(state_t *s) {
     return;

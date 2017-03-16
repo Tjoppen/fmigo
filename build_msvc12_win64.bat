2c1be7ef417e40ea62c05bd42fbf442d96c4b8da
Tomas Härdin <tomas.hardin@umu.se>
2017-03-16 16:48:05 +0100

    Make Win64 build work

--- build_msvc12_win64.bat
+++ build_msvc12_win64.bat
@@ -3,12 +3,13 @@
 @echo Prerequirements: cmake, Visual Studio 2013 (especially msbuild.exe)
 
 set CMAKE_BUILD_TYPE=Release
-set BUILD_OPTIONS=/verbosity:d /p:Configuration=%CMAKE_BUILD_TYPE%
+set BUILD_OPTIONS=/verbosity:m /p:Configuration=%CMAKE_BUILD_TYPE%
 set CMAKE_GENERATOR="Visual Studio 12 2013 Win64"
 set CMAKE_BUILD_TOOL=msbuild
+set BUILD_DIR=build_msvc12_win64
 
-mkdir build
-cd build
+mkdir %BUILD_DIR%
+cd %BUILD_DIR%
 cmake .. -G %CMAKE_GENERATOR% -DBUILD_FMUS=0 -DUSE_GPL=0 -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% && ^
 cmake --build .                  -- /m %BUILD_OPTIONS% && ^
 cmake --build . --target install -- %BUILD_OPTIONS%

6f3a9d137721f1e494b6acf350d5925ebaeaf376
Jonas Sandqvist <s4nk13@gmail.com>
2017-03-24 07:40:19 +0100

    use connection names instead of VR

--- truckmodel_scaina.sh
+++ truckmodel_scaina.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
 mpirun \
-    -np  5 fmigo-mpi -F headers_scania_model.txt -t 3 -d 0.001  -c 0,1,1,6 -c 1,9,0,3 \
-    -c 1,8,2,32 -c 2,31,1,7 -c 3,30,0,6 \
-    -c 2,33,3,27 -c 3,36,2,34 \
-    -p 2,7,1 -p 2,8,10 -p 2,17,1 -p 2,10,0.5 -p 2,11,1 \
-    -p 2,12,1 -p 2,16,1 -p 2,18,1 -p 2,30,1 \
-    -p 3,4,1 -p 3,8,0 \
+    -np  5 fmigo-mpi -F headers_scania_model.txt -t 3 -d 0.001  \
+    -c 0,omega,1,v_in \
+    -c 1,force_clutch,0,tau \
+    -c 1,v,2,w_shaft_in \
+    -c 2,f_shaft_out,1,force_in \
+    -c 3,omega,0,omega_l \
+    -c 2,w_wheel_out,3,v_in \
+    -c 3,f_c,2,f_wheel_in \
+    -p 2,gear_ratio,1 \
+    -p 2,tq_clutchMax,10 \
+    -p 2,tq_brake,1 \
+    -p 2,r_tire,0.5 \
+    -p 2,m_vehicle,1 \
+    -p 2,final_gear_ratio,1 \
+    -p 2,J_neutral,1 \
+    -p 2,ts,1 \
+    -p 2,k1,1 \
+    -p 3,r_w,1 \
+    -p 3,c_d,0 \
     ./kinematictruck/engine/engine.fmu \
     ./gsl2/clutch/clutch.fmu \
     ./gsl2/scania-driveline/drivetrain_G5IO_m_function_only.fmu \
-    ./gsl2/trailer/trailer.fmu > /tmp/f.csv
+    ./gsl2/trailer/trailer.fmu #> /tmp/f.csv

866ed3eaac62cd99b2a00694266b06c491e2efe3
Jonas Sandqvist <s4nk13@gmail.com>
2017-03-28 09:09:22 +0200

    nuked get local ip functions

--- ssp/ssp-launcher.py
+++ ssp/ssp-launcher.py
@@ -9,44 +9,6 @@ from lxml import etree
 import subprocess
 import shutil
 import psutil
-import socket
-
-if os.name != "nt":
-    import fcntl
-    import struct
-
-    def get_interface_ip(ifname):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s',
-                                ifname[:15]))[20:24])
-
-def get_lan_ip():
-    ip = socket.gethostbyname(socket.gethostname())
-    if ip.startswith("127.") and os.name != "nt":
-        interfaces = [
-            "eth0",
-            "eth1",
-            "eth2",
-            "wlan0",
-            "wlan1",
-            "wifi0",
-            "ath0",
-            "ath1",
-            "ppp0",
-            ]
-        for ifname in interfaces:
-            try:
-                ip = get_interface_ip(ifname)
-                break
-            except IOError:
-                pass
-    return ip
-
-if len(sys.argv) < 2:
-    #TODO: we probably want to know which of TCP and MPI is wanted
-    print('USAGE: %s [--dry-run --tcp:port1:port2:... --MPI] ssp-file' % sys.argv[0])
-    exit(1)
-
 RESOURCE_DIR='resources'
 SSD_NAME='SystemStructure.ssd'
 CAUSALITY='causality'
@@ -841,7 +803,6 @@ if __name__ == '__main__':
             print('WARNING: Given too many ports')
 
 
-        localhost = get_lan_ip()
         tcpIPport = []
         port = 1024 #lowest possible port
         for fmu in fmus:

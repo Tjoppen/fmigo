0223b3c06f7ee3188063a160dd07753a2ea60a7a
Jonas Sandqvist <s4nk13@gmail.com>
2017-04-06 13:32:14 +0200

    added compair files for model exchange

--- me/test_me.sh
+++ me/test_me.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-mpirun -np  2 fmigo-mpi -t 1.5 bouncingBall/bouncingBall.fmu
+SPRINGS=springs/springs.fmu
+mpirun -np  2 fmigo-mpi -t 1.5 bouncingBall/bouncingBall.fmu > /tmp/result
+cat /tmp/result
+echo Bouncing Ball ok
+mpirun -np 3 fmigo-mpi -t 12 -p r,1,11,1 -c 0,x0,1,1 ${SPRINGS} ${SPRINGS} > /tmp/result
+cat /tmp/result
 echo Bouncing Ball ok

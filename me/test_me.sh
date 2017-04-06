229c00ee1c14f5b4f873d8403c9a82bf59e771a9
Jonas Sandqvist <s4nk13@gmail.com>
2017-04-06 13:35:34 +0200

    update to test script

--- me/test_me.sh
+++ me/test_me.sh
@@ -1,9 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
+SPRINGSCHECK=tests/springs.txt
+BOUNCINGCHECK=tests/bouncingBall.txt
 SPRINGS=springs/springs.fmu
-mpirun -np  2 fmigo-mpi -t 1.5 bouncingBall/bouncingBall.fmu > /tmp/result
-cat /tmp/result
+mpirun -np 2 fmigo-mpi -t 1.5 bouncingBall/bouncingBall.fmu > /tmp/result 2>/dev/null
+DIFF=$(diff /tmp/result ${BOUNCINGCHECK})
+if [ "$DIFF" != "" ]
+then
+    cat /tmp/result
+    exit 1
+fi
 echo Bouncing Ball ok
-mpirun -np 3 fmigo-mpi -t 12 -p r,1,11,1 -c 0,x0,1,1 ${SPRINGS} ${SPRINGS} > /tmp/result
-cat /tmp/result
+
+mpirun -np 3 fmigo-mpi -t 12 -p r,1,11,1 -c 0,x0,1,1 ${SPRINGS} ${SPRINGS} > /tmp/result 2>/dev/null
+DIFF=$(diff /tmp/result ${SPRINGSCHECK})
+if [ "$DIFF" != "" ]
+then
+    cat /tmp/result
+    exit 1
+fi
+rm /tmp/result
 echo Bouncing Ball ok

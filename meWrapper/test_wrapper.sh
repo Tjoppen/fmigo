1ce6cc0e07c6a0d68d4e87173776475cf19eb66e
Jonas Sandqvist <s4nk13@gmail.com>
2017-04-07 09:22:28 +0200

    update test wrapper

--- meWrapper/test_wrapper.sh
+++ meWrapper/test_wrapper.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 set -e
 
+function test(){
 COMPARE=../../compare_csv.py
-WRAPPERCHECK=tests/wrapper.txt
-SPRINGSCHECK=tests/springs.txt
-WRAPPER=wrapper/wrapper_bouncingBall.fmu
 RESULT=/tmp/result.csv
+WRAPPER=$1
+CHECK=$2
 mpirun -np 2 fmigo-mpi -t 1.5 ${WRAPPER} > ${RESULT} 2>/dev/null
+cat  ${RESULT} > ${CHECK}
+python ${COMPARE} ${RESULT} ${CHECK}
+}
 
-cat  ${RESULT} > ${WRAPPERCHECK}
-python ${COMPARE} ${RESULT} ${WRAPPERCHECK}
-
+test wrapper/wrapper_bouncingBall.fmu tests/bouncingBall.csv
+test spring/wrapper_springs.fmu       tests/springs.csv
+test spring2/wrapper_springs2.fmu     tests/springs2.csv
 
 rm ${RESULT}
 echo Wrapper OK

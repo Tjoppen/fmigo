4ebe253ea3572e4c8caa5f40ff30d945c71679d5
Tomas Härdin <tomas.hardin@umu.se>
2017-04-07 09:07:44 +0200

    Add EPCE filter implementation

--- testfmus/filtertest/filtertest.py
+++ testfmus/filtertest/filtertest.py
@@ -1,13 +1,18 @@
 from scipy import integrate
 from math import atan, sqrt
+from numpy import array
 
 def fun(t, y, w_in, k1, d1, k2, d2, J1, J2):
-  dphi1, w1, dphi2, w2 = y
+  dphi1, w1, dphi2, w2 = y[0:4]
   return [
     w_in - w1,
     (k1*dphi1 + d1*(w_in - w1) - k2*dphi2 - d2*(w1 - w2))/J1,
     w1 - w2,
     (k2*dphi2 + d2*(w1   - w2))/J2,
+    dphi1,
+    w1,
+    dphi2,
+    w2,
   ]
 
 w_in = 10.0
@@ -22,16 +27,27 @@ d_internal = 1.4*sqrt(J/2 * c_internal)
 c = 1e5
 d = 1.4*sqrt(c)
 
-r = integrate.ode(fun).set_initial_value([0,0,0,0], 0).set_f_params(w_in, c, d, c_internal, d_internal, J/2, J/2)
+r = integrate.ode(fun).set_initial_value([0.0]*8, 0).set_f_params(w_in, c, d, c_internal, d_internal, J/2, J/2)
 t1 = 0.1
-dt = 0.001
+dt = 0.03
 
-solt = [0]
-sol = [r.y]
-print('%f %s' % (r.t, str(r.y)))
+ts   = [0]
+ys   = [r.y[0:4]]
+zs   = [r.y[4:8]]
+zs2  = [r.y[4:8]]
+
 while r.successful() and r.t < t1:
+  r.set_initial_value([r.y[0], r.y[1], r.y[2], r.y[3]] + [0.0]*4, r.t)
   r.integrate(r.t + dt)
-  print('%f %s' % (r.t, str(r.y)))
-  solt.append(r.t)
-  sol.append(r.y)
 
+  ts.append(r.t)
+  ys.append(r.y[0:4])
+  zs.append(r.y[4:8]/dt)
+  zs2.append((zs[-1] + zs[-1 if len(zs) == 1 else -2]) / 2)
+
+# lists if ndarrays aren't slizeable, so convert to array
+ys = array(ys)
+zs = array(zs)
+zs2 = array(zs2)
+
+#plot(ts, ys, ts, zs, ts, zs2)

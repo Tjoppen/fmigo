f2ae61f50ef7395775cfd813f483769ffe8c5e81
Tomas Härdin <tomas.hardin@umu.se>
2017-04-12 17:01:13 +0200

    Do blocking recv on Windows. poll() is broken

--- fmigo/src/master/BaseMaster.cpp
+++ fmigo/src/master/BaseMaster.cpp
@@ -182,6 +182,17 @@ void BaseMaster::wait() {
 
         m_clients[rank-1]->Client::clientData(str.c_str(), str.length());
 #else
+#ifdef WIN32
+    //zmq::poll() is broken and incredibly slow on Windows
+    //this is the stupidest possible solution, but works surprisingly well
+    //it can't detect that a server has croaked however
+    for (auto client : m_clients) {
+        if (client->getNumPendingRequests() > 0) {
+            client->receiveAndHandleMessage();
+        }
+    }
+#else
+    //all other platforms (GNU/Linux, Mac)
     //poll all clients, decrease m_pendingRequests as we see REPlies coming in
     vector<zmq::pollitem_t> items(m_clients.size());
     for (size_t x = 0; x < m_clients.size(); x++) {
@@ -212,6 +223,7 @@ void BaseMaster::wait() {
     } else {
         //debug("wait() done\n");
     }
+#endif
 #endif
     }
 }

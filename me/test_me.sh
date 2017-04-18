aede2e2d0c404f1cc7385f0a16ff964b48e51da6
Tomas Härdin <tomas.hardin@umu.se>
2017-04-18 11:00:27 +0200

    Make Windows compatible

--- me/test_me.sh
+++ me/test_me.sh
@@ -3,21 +3,21 @@
 SPRINGSCHECK=tests/springs.txt
 BOUNCINGCHECK=tests/bouncingBall.txt
 SPRINGS=springs/springs.fmu
-mpirun -np 2 fmigo-mpi -t 1.5 bouncingBall/bouncingBall.fmu > /tmp/result 2>/dev/null
-DIFF=$(diff /tmp/result ${BOUNCINGCHECK})
+mpiexec -np 2 fmigo-mpi -t 1.5 bouncingBall/bouncingBall.fmu > result 2>/dev/null
+DIFF=$(diff result ${BOUNCINGCHECK})
 if [ "$DIFF" != "" ]
 then
-    cat /tmp/result
+    cat result
     exit 1
 fi
 echo Bouncing Ball ok
 
-mpirun -np 3 fmigo-mpi -t 12 -p r,1,11,1 -c 0,x0,1,1 ${SPRINGS} ${SPRINGS} > /tmp/result 2>/dev/null
-DIFF=$(diff /tmp/result ${SPRINGSCHECK})
+mpiexec -np 3 fmigo-mpi -t 12 -p r,1,11,1 -c 0,x0,1,1 ${SPRINGS} ${SPRINGS} > result 2>/dev/null
+DIFF=$(diff result ${SPRINGSCHECK})
 if [ "$DIFF" != "" ]
 then
-    cat /tmp/result
+    cat result
     exit 1
 fi
-rm /tmp/result
+rm result
 echo Bouncing Ball ok

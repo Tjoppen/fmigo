23c7b29679d1bc1a7dd1369af0dbdd80710da3cb
Tomas Härdin <tomas.hardin@umu.se>
2017-07-04 11:55:06 +0200

    Don't include string.h more than once
    Makes the code a bit neater

--- umit-fmus/templates/fmi2/strlcpy.h
+++ umit-fmus/templates/fmi2/strlcpy.h
@@ -6,8 +6,6 @@
 //it's not the most efficient, but it gets the job done
 #ifndef HAVE_STRLCPY  //try to play nice with a hypothetical future build system
 #define HAVE_STRLCPY
-#include <string.h> //for strlen() and memcpy()
-
 static size_t strlcpy(char *dst, const char *src, size_t size) {
     size_t ret = strlen(src);
     size_t bytes = ret < size-1 ? ret : size-1;
@@ -19,8 +17,6 @@ static size_t strlcpy(char *dst, const char *src, size_t size) {
 
 #ifndef HAVE_STRLCAT
 #define HAVE_STRLCAT
-#include <string.h>
-
 static size_t strlcat(char *dst, const char *src, size_t size) {
     size_t a = strlen(dst);
     size_t b = strlen(src);

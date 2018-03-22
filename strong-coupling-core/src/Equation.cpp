ab3d7255df99a8836a59a92733d68dae140600d0
Tomas Härdin <tomas.hardin@umu.se>
2018-03-22 13:56:47 +0100

    Finish generalizing Equation and add MultiWayConstraint

--- strong-coupling-core/src/Equation.cpp
+++ strong-coupling-core/src/Equation.cpp
@@ -3,11 +3,18 @@
 using namespace sc;
 using namespace std;
 
-Equation::Equation(const vector<Connector*>& connectors) : m_connectors(connectors) {
+Equation::Equation(const vector<Connector*>& connectors) :
+        m_Gs(connectors.size(), JacobianElement()),
+        m_connectors(connectors) {
+    if (connectors.size() < 2) {
+        fprintf(stderr, "Too few connectors in Equation\n");
+        exit(1);
+    }
     setDefault();
 }
 
-Equation::Equation(Connector * connA, Connector * connB){
+Equation::Equation(Connector * connA, Connector * connB) :
+        m_Gs(2, JacobianElement()) {
     m_connectors.push_back(connA);
     m_connectors.push_back(connB);
 
@@ -27,12 +34,6 @@ void Equation::setRelativeVelocity(double v){
     m_relativeVelocity = v;
 }
 
-void Equation::setDefaultViolation(){
-    Vec3 zero;
-    m_g = m_G_A.multiply(m_connectors[0]->m_position, zero) +
-          m_G_B.multiply(m_connectors[1]->m_position, zero);
-}
-
 void Equation::setViolation(double g){
     m_g = g;
 }
@@ -41,20 +42,31 @@ void Equation::setG(double sxA, double syA, double szA,
                     double rxA, double ryA, double rzA,
                     double sxB, double syB, double szB,
                     double rxB, double ryB, double rzB){
-    m_G_A.setSpatial    (sxA, syA, szA);
-    m_G_A.setRotational (rxA, ryA, rzA);
-    m_G_B.setSpatial    (sxB, syB, szB);
-    m_G_B.setRotational (rxB, ryB, rzB);
+    m_Gs[0].setSpatial    (sxA, syA, szA);
+    m_Gs[0].setRotational (rxA, ryA, rzA);
+    m_Gs[1].setSpatial    (sxB, syB, szB);
+    m_Gs[1].setRotational (rxB, ryB, rzB);
 }
 
 void Equation::setG(const Vec3& spatialA,
                     const Vec3& rotationalA,
                     const Vec3& spatialB,
                     const Vec3& rotationalB){
-    m_G_A.setSpatial(spatialA);
-    m_G_A.setRotational(rotationalA);
-    m_G_B.setSpatial(spatialB);
-    m_G_B.setRotational(rotationalB);
+    m_Gs[0].setSpatial(spatialA);
+    m_Gs[0].setRotational(rotationalA);
+    m_Gs[1].setSpatial(spatialB);
+    m_Gs[1].setRotational(rotationalB);
+}
+
+void Equation::setG(size_t i,
+                    double sxA, double syA, double szA,
+                    double rxA, double ryA, double rzA) {
+    if (i < 0 || i >= m_Gs.size()) {
+        fprintf(stderr, "Equation::setG(): i out of range\n");
+        exit(1);
+    }
+    m_Gs[i].setSpatial    (sxA, syA, szA);
+    m_Gs[i].setRotational (rxA, ryA, rzA);
 }
 
 bool Equation::haveOverlappingFMUs(Equation *other) const {

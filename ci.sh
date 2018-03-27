19f27d07be4632dde1986481ead6287f0439385b
Tomas Härdin <tomas.hardin@umu.se>
2018-03-27 15:49:15 +0200

    Test xenial first, catches errors faster

--- ci.sh
+++ ci.sh
@@ -3,7 +3,7 @@
 # For some reason there isn't a convenient command for running
 # all jobs in the proper order (build -> test), hence this script.
 set -e
-for t in trusty trusty-cmake3.5.0 xenial
+for t in xenial trusty trusty-cmake3.5.0
 do
   gitlab-ci-multi-runner exec shell build:$t && gitlab-ci-multi-runner exec shell test:$t || exit $?
 done
